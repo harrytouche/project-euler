80a7460296ef08e00be8da371a8100692e2cae01
Harry Touche <harrytouche@gmail.com>
2020-01-13 10:59:30 +0000

    problem 15

--- 015/main.py
+++ 015/main.py
@@ -6,17 +6,33 @@ How many such routes are there through a 20×20 grid?
 
 import numpy as np
 
-side = 2
+grid_size = 20
 
 # create grid
+side = grid_size + 1
 grid = np.zeros((side,side), dtype=int)
+grid.fill(1)
 
 # loop through grid from top right
-for i in range(side):
+for i in range(side*2):
     
-    grid_x = i
-    grid_y = side-i
+    #print("i = {}".format(i))
+    
+    for j in range(i+1):
+        grid_x = j
+        grid_y = i-j
+
+        """
+        print("{} / {}".format(j, i))
+        print("grid_x = {}".format(grid_x))
+        print("grid_y = {}".format(grid_y))
+        input("Press enter to proceed")
+        """
+                
+        if grid_x != 0 and grid_y != 0 and grid_x < side and grid_y < side:
+            grid[grid_x, grid_y] = grid[grid_x-1, grid_y] + grid[grid_x, grid_y-1]
             
-    grid[grid_x, grid_y] = grid[grid_x-1, grid_y] + grid[grid_x, grid_y-1]
+        
     
+#print(grid)
 print(grid[-1,-1])
