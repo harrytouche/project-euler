a20ad61a3deb6e842c813e392c095ab87b999c28
Harry Touche <harrytouche@gmail.com>
2020-01-15 13:16:37 +0000

    Update main.py

--- 016/main.py
+++ 016/main.py
@@ -6,3 +6,17 @@ Created on Wed Jan 15 13:00:18 2020
 @author: hartouche
 """
 
+from functools import reduce
+
+
+def SumTwoNumbersAsInt(a,b):
+    return int(a) + int(b)
+
+def SumOfDigitsOfNumber(n):
+
+    n_sum = reduce(SumTwoNumbersAsInt, list(str(n)))
+    
+    return n_sum
+
+print(SumOfDigitsOfNumber(2**1000))
+        
